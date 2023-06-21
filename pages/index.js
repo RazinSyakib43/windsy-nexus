cddf1fa420c4bee7151fe12bd4c8bc9f793b4dc3
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-21 23:43:18 +0700

    create app.yaml and update .env

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ import design from '../public/design.png';
 import code from '../public/code.png';
 import consulting from '../public/consulting.png';
 import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 import { useState } from 'react';
 
 export default function Home() {
@@ -89,11 +94,11 @@ export default function Home() {
           </div>
           <div className='flex flex-col gap-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
-            <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
-            <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
-            <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
-            <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
-            <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
+            <div className='basis-1/3 flex-1'><Image src={web2} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
+            <div className='basis-1/3 flex-1'><Image src={web3} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
+            <div className='basis-1/3 flex-1'><Image src={web4} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
+            <div className='basis-1/3 flex-1'><Image src={web5} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
+            <div className='basis-1/3 flex-1'><Image src={web6} className='rounded-lg object-cover width={"100%"} height={"100%"} layout="responsive"' /></div>
           </div>
         </section>
       </main>
